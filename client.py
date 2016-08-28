3db70ee5c8a558c932e380d6e81ee976f55a2307
dsztanko <szt.dori@gmail.com>
2016-08-28 15:39:13 +0200

    adding oop working logic to client.py

--- client.py
+++ client.py
@@ -1,30 +1,97 @@
 import argparse
 import ipaddress
+import requests
+import datetime
+import os
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-H", "--host",
-                    help="IP address of the Tweetcool server",
-                    default='127.0.0.1')  # Equals 'localhost'
-parser.add_argument("-P", "--port",
-                    help="Post used by the Tweetcool server",
-                    type=int,
-                    default=9876)
-args = parser.parse_args()
+class Client(object):
 
-try:
-    server = {
-        'host': ipaddress.ip_address(args.host),
-        'port': args.port
-    }
-except ValueError as e:
-    print('The given host is not a valid IP address')
-    exit(0)
+    def __init__(self):
+        self.user_name = input('Please, add your user name to log in: ')
 
-if not(1024 < server["port"] < 65535):
-    print('The given port number is not in the range between 1024 and 65535!')
-    exit(0)
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-H", "--host",
+                            help="IP address of the Tweetcool server",
+                            default='127.0.0.1')  # Equals 'localhost'
+        parser.add_argument("-P", "--port",
+                            help="Post used by the Tweetcool server",
+                            type=int,
+                            default=9876)
+        args = parser.parse_args()
 
-server["address"] = 'http://' + server["host"].compressed + ':' + str(server["port"])
+        try:
+            self.server = {
+                'host': ipaddress.ip_address(args.host),
+                'port': args.port
+            }
+        except ValueError as e:
+            print('The given host is not a valid IP address')
+            exit(0)
 
-# Logic starts here... somewhere..
+        if not(1024 < self.server["port"] < 65535):
+            print('The given port number is not in the range between 1024 and 65535!')
+            exit(0)
+
+        self.server["address"] = 'http://' + self.server["host"].compressed + ':' + str(self.server["port"])
+        self.route = self.server["address"] + "/tweet"
+
+        self.menu()
+
+    def post_tweet(self, message):
+        payload = {"content": str(message), "poster": self.user_name}
+        requests.post(self.route, json=payload)
+
+    @staticmethod
+    def display_goodbye_message():
+        print('Thanks for using TWEETCOOL!\n')
+
+    def menu(self):
+        while True:
+            try:
+                option = input('''
+---------------- TWITTERCOOL ------------------
+{0}
+Enter:
+    tweet -- to post your thoughts;
+    refresh -- to refresh the timeline;
+    exit or CTRL+D -- to quit the application.
+{0}
+
+Option: '''.format(47*'-'))
+
+                if option == 'refresh':
+                    os.system('clear')
+                    r = requests.get(self.route)
+                    requested_data = r.json()
+                    print('>>> TIMELINE')
+                    for tweet in requested_data:
+                        print('>>>{0} <{1}>: {2}'.format(
+                            tweet['poster'],
+                            datetime.datetime.fromtimestamp(int(tweet['timestamp'])).strftime('%Y-%m-%d %H:%M:%S'),
+                            tweet['content'])
+                                                        )
+                elif option == 'tweet':
+                    try:
+                        self.post_tweet(input('Post a tweet: '))
+                    except UnicodeDecodeError as e:
+                        print(e)
+                        exit()
+                elif option == 'exit':
+                    self.display_goodbye_message()
+                    exit()
+                else:
+                    print('Not a valid option. Try again!')
+
+            except EOFError:
+                self.display_goodbye_message()
+                exit()
+
+            except KeyboardInterrupt:
+                self.display_goodbye_message()
+                exit()
+
+
+client = Client()
+
+
